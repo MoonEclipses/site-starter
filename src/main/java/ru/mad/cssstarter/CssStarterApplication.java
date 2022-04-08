05cf60236da9e18bb52d215c84f72bcb8b4b303a
MoonEclipses <nagorny.omsk@mail.ru>
2022-04-08 14:07:06 +0600

    Project setted up

---
+++ src/main/java/ru/mad/cssstarter/CssStarterApplication.java
@@ -0,0 +1,13 @@
+package ru.mad.cssstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mad/cssstarter/CssStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mad.cssstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
