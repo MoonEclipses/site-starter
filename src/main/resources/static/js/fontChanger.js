2e39a7fa87eada48b0ce799487121df72e102793
Bl1tzed <dns-2001@mail.ru>
2022-04-17 21:23:36 +0600

    Добавлены все шрифты Google, заменил select на input+datalist

--- src/main/resources/static/js/fontChanger.js
+++ src/main/resources/static/js/fontChanger.js
@@ -1,8 +1,14 @@
-let list = document.querySelector('#list'); //Берем лист
+let list = document.querySelector('#selectedFont'); //Берем лист
 const toChange = document.querySelector('.toChange'); //Берем текст, который будем менять
 
+
+
+list.addEventListener("click", function () {
+    list.value = "";
+
+})
 list.addEventListener("change", change) //При изменении в листе меняем текст
 
 function change() {
-    toChange.style.fontFamily = list.options[list.selectedIndex].text;
+    toChange.style.fontFamily = list.value;
 }
--- src/main/resources/static/js/fontLoader.js
+++ src/main/resources/static/js/fontLoader.js
@@ -2,13 +2,20 @@ document.addEventListener("DOMContentLoaded", ready)
 
 function ready() {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', '/fonts/get');
+    //ссылка https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCpITb3IwHT5c99X-0Cj54hp8iL_D8-fAE
+    xhr.open('GET', 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCpITb3IwHT5c99X-0Cj54hp8iL_D8-fAE');
+    //вместо ссылки при запуске серва должно стоять /fonts/get
     xhr.responseType = 'json';
     xhr.send();
+    let datalist = document.getElementById('datalist');
 
     xhr.onload = function () {
-        let responseObj = xhr.response;
-        let fonts = JSON.parse(responseObj);
-        alert(fonts.items[0].family);
+        let fonts = xhr.response;
+        for (let i = 0; i < fonts.items.length; i++){
+            let option = document.createElement('option');
+            option.value = fonts.items[i].family;
+            datalist.appendChild(option);
+
+        }
     };
 }
